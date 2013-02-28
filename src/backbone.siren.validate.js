8584f48b5312968dbcf4f412e0f036db1bc398e7
uglymunky <gabriel@uglymunky.com>
2013-02-28 13:19:15 -0800

    First stab at adding a validation module...none of it works yet

---
+++ src/backbone.siren.validate.js
@@ -0,0 +1,102 @@
+/*global _ Backbone */
+
+/*
+ * Backbone.Siren.Validate
+ *
+ * Copyright (c) 2013 Kiva Microfunds
+ * Licensed under the MIT license.
+ * https://github.com/kiva/backbone.siren/blob/master/license.txt
+ */
+(function(root, factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['underscore', 'backbone.siren'], function(_, Backbone) {
+            factory(_, Backbone);
+        });
+    } else {
+        // Browser globals
+        factory(_, Backbone);
+    }
+
+}(this, function (_, Backbone, undefined) {
+    'use strict';
+
+
+    _.extend(Backbone.Siren.Model, {
+
+
+        /**
+         * Override the default _validate method so we only get changed attributes instead of all attributes
+         * More info: https://github.com/documentcloud/backbone/pull/1595
+         *
+         * @param {Object} attrs
+         * @param {Object} options
+         * @return {Boolean}
+         * @private
+         */
+        _validate: function (attrs, options) {
+            var error;
+
+            if (!options.validate || !this.validate) {
+                return true;
+            }
+
+            // This is the line that we are removing from the default implementation
+            // attrs = _.extend({}, this.attributes, attrs);
+            error = this.validationError = this.validate(attrs, options) || null;
+            if (!error) {
+                return true;
+            }
+
+            this.trigger('invalid', this, error, options || {});
+            return false;
+        }
+
+
+        /**
+         *
+         */
+        , validateOne: function (/*val, field, options*/) {
+            // @todo
+            return '';
+        }
+
+
+        /**
+         * See http://backbonejs.org/#Model-validate
+         *
+         * @param {Object} attributes
+         * @param {Object} options
+         */
+        , validate: function (attributes, options) {
+            var self = this
+            , errors = this.errors = {};
+
+            if (_.isEmpty(attributes)) {
+                return errors['non-writable-fields'] = 'There were no writable fields, check your siren action. @todo better messaging';
+            }
+
+            var action = this.action(options.actionName);
+            if (!action) {
+                return errors['non-writable-fields'] = 'There were no writable fields, check your siren action. @todo better messaging';
+            }
+
+            _.each(attributes, function (val, name) {
+                if (val instanceof Backbone.Model) {
+                    // @todo iterate through and validate sub-entity actions.
+                    console.log('@todo');
+                } else {
+                    var errorMsg = self.validateOne(val, action.field(name), options);
+                    if (errorMsg) {
+                        errors[name] = errorMsg;
+                    }
+                }
+            });
+
+            return errors;
+        }
+
+    });
+}));
