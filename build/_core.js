9a67ad0ae94a4d041ee1c8be5906eb7753e3ca77
uglymunky <gabriel@uglymunky.com>
2013-04-24 11:39:19 -0700

    automatically set the pattern library

--- build/_core.js
+++ build/_core.js
@@ -2,6 +2,10 @@
 
 //= ../src/backbone.siren.validate.js
 
+//= ../src/patternLibrary.js
+
+Backbone.Siren.validate.setPatterns(patternLibrary);
+
 //= ../src/backbone.siren.errors.js
 
 //= ../src/backbone.siren.formView.js
