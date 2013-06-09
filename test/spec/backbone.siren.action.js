kiva/backbone.siren

commit ec7be61e48a73aa36fa3e4dada80f0162767c992

first stab at not setting action fields on the actual model

src/backbone.siren.js
@@ -130,7 +130,18 @@ Backbone.Siren = (function (_, Backbone, undefined) {
                 attributes = parent.getAllByAction(this.name);
             }
 
-            return parent.save(attributes, options);
+            // Create a temporary clone that will house all our actions related properties
+            var actionModel = this.actionModel = parent.clone();
+            actionModel._data = parent._data;
+            actionModel._actions = parent._actions;
+
+            options.success = function (model, resp, options) {
+                parent.trigger('sync', model, resp, options);
+                parent.attributes = {};
+                parent.set(actionModel.attributes);
+            };
+
+            return actionModel.save(attributes, options);
         }
     };
 

commit 037b51b8634dba436855223cbc906ceeb69efa0b

fix validation bug that was introduced in ec7be61 + add test

src/backbone.siren.js
@@ -170,7 +170,7 @@ Backbone.Siren = (function (_, Backbone, undefined) {
         , execute: function (options) {
             options = options || {};
 
-            var actionModel
+            var actionModel, jqXhr
             , attributes = options.attributes
             , actionName = this.name
             , parent = this.parent
@@ -208,7 +208,13 @@ Backbone.Siren = (function (_, Backbone, undefined) {
 
             options = _.extend(presets, options);
             attributes = _.extend(parent.getAllByAction(this.name), attributes);
-            return actionModel.save(attributes, options);
+
+		    jqXhr = actionModel.save(attributes, options);
+
+		    // Transfer any validation errors back onto the "original" model
+		    parent.validationError = actionModel.validationError;
+
+		    return jqXhr;
         }
     };
 

test/spec/backbone.siren.action.js
@@ -25,7 +25,7 @@ describe('Siren Action: ', function () {
     });
 
 
-    describe('.execute', function () {
+    describe('.execute()', function () {
         beforeEach(function () {
             this.stub($, 'ajax').returns('jqXhr');
         });
@@ -61,6 +61,17 @@ describe('Siren Action: ', function () {
         it('returns undefined if there is no parent to the action', function () {
             expect(bbSirenAction.execute()).not.toBeDefined();
         });
+
+
+	    it('sets the validationError object on models that fail validation', function () {
+		    var mySirenModel = {href: 'test', actions: [sirenAction]}
+			, myBbSirenModel = new Backbone.Siren.Model(mySirenModel);
+
+		    this.stub(Backbone.Siren.Model.prototype, 'validate').returns('There was an error');
+
+		    myBbSirenModel.getActionByName('add-item').execute();
+		    expect(myBbSirenModel.validationError).toMatch('There was an error');
+	    });
     });
 });
 