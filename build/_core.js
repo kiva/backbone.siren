a184b9ccc2a233d9cd2e2c66d17214164e4cb0cb
uglymunky <gabriel@uglymunky.com>
2013-06-29 23:51:00 -0700

    create new build w/o validation or formview

--- build/_core.js
+++ build/_core.js
@@ -1,11 +1,17 @@
 //= ../src/backbone.siren.js
 
-//= ../src/backbone.siren.validate.js
+/*
 
-//= ../src/patternLibrary.js
+@todo These need more work.
+
+// ../src/backbone.siren.validate.js
+
+// ../src/patternLibrary.js
 
 Backbone.Siren.validate.setPatterns(patternLibrary);
 
-//= ../src/backbone.siren.errors.js
+// ../src/backbone.siren.errors.js
+
+*/
 
 //= ../src/backbone.siren.formView.js
