9c5a515ee6c9d0707fbd1e27647fc8706421750a
uglymunky <gabriel@uglymunky.com>
2013-12-03 11:17:07 -0800

    bump v0.2.16

--- dist/amd/backbone.siren.js
+++ dist/amd/backbone.siren.js
@@ -250,6 +250,10 @@ define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
                     presets.contentType = this.type;
                 }
     
+    		    if (presets.type == 'PATCH') {
+    			    options.patch = true;
+    		    }
+    
     		    // Create a temporary clone that will house all our actions related properties
     		    // We do this because Backbone will override our model with the response from the server
     		    // @todo we probably want something smarter so that we can update the model but still mitigate funky stuff from happening in the View.
