e2771c02b3f186bbb50acb2f1819243a23b9c288
uglymunky <gabriel@uglymunky.com>
2014-05-02 15:54:38 -0700

    comment out dist tests

--- test/buster.js
+++ test/buster.js
@@ -24,30 +24,30 @@ config.development = {
 
 
 // @todo - enable these
+//
+//config.dist_amd = {
+//    env: 'browser'
+//    , rootPath: '../'
+//    , deps: [
+//		'test/assertions.js'
+//        , 'bower_components/jquery/jquery.js'
+//        , 'bower_components/underscore/underscore.js'
+//        , 'bower_components/backbone/backbone.js'
+//    ]
+//    , sources: ['dist/amd/backbone.siren.min.js']
+//    , tests: ['test/spec/**/*.js']
+//};
 
-config.dist_amd = {
-    env: 'browser'
-    , rootPath: '../'
-    , deps: [
-		'test/assertions.js'
-        , 'bower_components/jquery/jquery.js'
-        , 'bower_components/underscore/underscore.js'
-        , 'bower_components/backbone/backbone.js'
-    ]
-    , sources: ['dist/amd/backbone.siren.min.js']
-    , tests: ['test/spec/**/*.js']
-};
-
-
-config.dist_iife = {
-	env: 'browser'
-	, rootPath: '../'
-	, deps: [
-		'test/assertions.js'
-		, 'bower_components/jquery/jquery.js'
-		, 'bower_components/underscore/underscore.js'
-		, 'bower_components/backbone/backbone.js'
-	]
-	, sources: ['dist/iife/backbone.siren.min.js']
-	, tests: ['test/spec/**/*.js']
-};
+//
+//config.dist_iife = {
+//	env: 'browser'
+//	, rootPath: '../'
+//	, deps: [
+//		'test/assertions.js'
+//		, 'bower_components/jquery/jquery.js'
+//		, 'bower_components/underscore/underscore.js'
+//		, 'bower_components/backbone/backbone.js'
+//	]
+//	, sources: ['dist/iife/backbone.siren.min.js']
+//	, tests: ['test/spec/**/*.js']
+//};
